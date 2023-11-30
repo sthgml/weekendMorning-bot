afbd2ae8abaa3da52a5b27c99c3a2e64e41a8998
sohee <sthgml@naver.com>
2023-11-30 14:22:06 +0900

    ♻️ refactor : 함수 분리 + luxon local time 사용

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 require("./server.js");
-
-const dotenv = require("dotenv");
-dotenv.config();
-
 const { Client, Collection } = require("discord.js");
-const { DateTime } = require("luxon");
-const mySecret = process.env['TOKEN'];
 const client = (module.exports = new Client({ intents: [131071] }));
+const { DateTime } = require("luxon");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const mySecret = process.env['TOKEN'];
 client.login(mySecret); //봇토큰
 
-client.once("ready", () => {
-  console.log(`${client.user.tag} 로그인`);
-
-  // 실행시간 기준으로 다음 시간 계산
-  const now = DateTime.now();
-
-  // 다음 날 오전 9시를 계산
-  // 현재 요일에서  일수를 더하고, 
-  // 시간을 09:00으로 설정합니다. (근디 서버시간이 한국이랑 9시간 시차 있음)
+const getNextDay = () => {
+  const now = DateTime.now().setLocale('kr');
   const nextDay = now
-    .plus({ days: (now.weekday + 1) % 7 })
+    .plus({ days: 1 })
     .set({ hour: 0, minute: 0, second: 0 });
+  return { now, nextDay };
+}
+
+const getNextDelay = () => {
+  const { now, nextDay } = getNextDay();
 
   // 다음 날 메시지를 보낼 때까지 
   // 대기해야 하는 밀리초 단위의 시간을 계산합니다.
@@ -33,32 +30,32 @@ client.once("ready", () => {
   }
 
   console.log(delay);
+  return delay;
+}
 
-  // 다음 실행 예약
+const setNextTimeout = () => {
+  let delay = getNextDelay();
   setTimeout(() => {
     createThread();
   }, delay);
-})
+};
 
 function createThread() {
   client.channels
     .fetch("1161169358794530856") // 채널id
     .then(async (channel) => {
-      // console.log(channel.name);
-
-      let date = new Date();
+      const { nextDay } = getNextDay();
+      const nextDayString = nextDay.toFormat("M'월' d'일'");
 
       const message = await channel.send({
-        content: `${date.getMonth() + 1}월 ${date.getDate() + 1}일 스터디 참여 여부 조사`
+        content: `${nextDayString} 스터디 참여 여부 조사`
       });
 
       const thread = await message.startThread({
-        name: `${date.getMonth() + 1}월 ${date.getDate() + 1}일 스터디 참여 여부 조사`,
+        name: `${nextDayString} 스터디 참여 여부 조사`,
         autoArchiveDurateion: 60,
         reason: '평일 오전 할 일 하기 스터디 리마인딩, 불참 사유 조사',
       });
-
-      // console.log(`Created thread: ${thread.id}`);
 
       channel.threads.fetch;
       const webhooks = await channel.fetchWebhooks();
@@ -73,21 +70,23 @@ function createThread() {
         threadId: thread.id,
       });
 
-      // 다시 실행 예약
-      delay = 86400000;
-      setTimeout(() => {
-        createThread();
-      }, delay);
+      // 다음 실행 예약
+      setNextTimeout();
     })
     .catch(console.error);
 }
 
+client.once("ready", () => {
+  console.log(`${client.user.tag} 로그인`);
+
+  setNextTimeout();
+})
+
 client.on("messageCreate", (message) => {
   if (message.content == "쓰레드만들기") {
-    // message.reply({ content: "**반갑습니다**" });
     createThread();
   }
   if (message.content == "안녕") {
     message.reply({ content: "**반갑습니다**" });
   }
-})
+});
